7e5c8133988be60057520b5ca8146b14e1da098a
Lorenzo Alberton <l.alberton@quipo.it>
2012-03-13 13:32:52 +0000

    increment library version in user agent string

--- src/org/datasift/User.java
+++ src/org/datasift/User.java
@@ -22,7 +22,7 @@ public class User {
 	 *
 	 * @access public
 	 */
-	public final static String _user_agent = "DataSiftJava/1.3.0";
+	public final static String _user_agent = "DataSiftJava/1.3.2";
 
 	/**
 	 * The base URL for API calls. No http://, and with the trailing slash.
