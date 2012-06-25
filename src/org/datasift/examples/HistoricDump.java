b401f9348f500a61f010281569714cbc5b90cf2b
Stuart Dallas <github@3ft9.com>
2012-06-25 13:27:18 +0100

    Added CLI arguments to the HistoricDump example and changed it to write the CSV out to a file rather than stdout.

--- src/org/datasift/examples/HistoricDump.java
+++ src/org/datasift/examples/HistoricDump.java
@@ -3,6 +3,8 @@
  */
 package org.datasift.examples;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,33 +16,49 @@ import org.datasift.*;
  * @version 0.1
  */
 public class HistoricDump implements IStreamConsumerEvents {
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		if (args.length != 5) {
+			System.err.println("Usage: HistoricDump <keywords> <start_date> <end_date> <feeds> <csv_filename>");
+			System.err.println("Where...");
+			System.err.println("  <keywords>     comma separated list of keywords");
+			System.err.println("  <start_date>   start date (yyyymmddhhmmss)");
+			System.err.println("  <end_date>     end date (yyyymmddhhmmss)");
+			System.err.println("  <feeds>        comma separated list of feeds (twitter,digg,etc)");
+			System.err.println("  <csv_filename> filename for the csv output");
+			System.exit(1);
+		}
+
 		// Config
-		String csdl  = "interaction.type == \"twitter\"";
-		String start = "2012-05-23 12:00:00";
-		String end   = "2012-05-23 13:00:00";
-		String feeds = "twitter";
-		
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		
+		String csdl = "interaction.type == \"twitter\" and language.tag == \"en\" and interaction.content contains_any \""
+				+ args[0].replace("\"", "\\\"") + "\"";
+
+		DateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+
 		try {
 			// Authenticate
 			System.err.println("Creating user...");
-			User user = new User(Config.username, Config.api_key, false);
+			User user = new User(Config.username, Config.api_key);
 
 			// Create the definition
+			System.err.println("Creating definition...");
 			Definition d = user.createDefinition(csdl);
-			
+
 			// Create the historic
-			Historic h = d.createHistoric(df.parse(start), df.parse(end), feeds, 100);
-			
+			System.err.println("Creating historic...");
+			Historic h = d.createHistoric(df.parse(args[1]), df.parse(args[2]),
+					args[3], 100);
+
+			// Display the playback ID
+			System.err.println("Playback ID: " + h.getHash());
+
 			// Create the consumer
 			System.err.println("Getting the consumer...");
-			StreamConsumer consumer = h.getConsumer(StreamConsumer.TYPE_HTTP, new HistoricDump(h));
+			StreamConsumer consumer = h.getConsumer(StreamConsumer.TYPE_HTTP,
+					new HistoricDump(h, args[4]));
 
 			// And start consuming
 			System.err.println("Consuming...");
@@ -63,21 +81,31 @@ public class HistoricDump implements IStreamConsumerEvents {
 			System.err.println(e.getMessage());
 		}
 	}
-	
-	private Historic _h = null;
-	
-	public HistoricDump(Historic h) {
-		_h = h;
-	}
 
+	private Historic _h = null;
+	private FileWriter _fw = null;
+	private long _counter = 0L;
+
+	public HistoricDump(Historic h, String fn) {
+		_h = h;
+		try {
+			_fw = new FileWriter(fn);
+			_fw.append("ID,Created At,Username,Content\n");
+		} catch (IOException e) {
+			System.err.println("ERR: Failed to create and/or write to the CSV file: " + e.getMessage());
+			System.exit(1);
+		}
+	}
+	
 	/**
 	 * Called when the connection has been established.
 	 * 
-	 * @param StreamConsumer consumer The consumer object.
+	 * @param StreamConsumer
+	 *            consumer The consumer object.
 	 */
 	public void onConnect(StreamConsumer c) {
 		System.err.println("Connected, starting historic query");
-		
+
 		// Start the historic query
 		try {
 			_h.start();
@@ -93,11 +121,12 @@ public class HistoricDump implements IStreamConsumerEvents {
 			System.err.println(e.getMessage());
 		}
 	}
-	
+
 	/**
 	 * Called when the connection has disconnected.
 	 * 
-	 * @param StreamConsumer consumer The consumer object.
+	 * @param StreamConsumer
+	 *            consumer The consumer object.
 	 */
 	public void onDisconnect(StreamConsumer c) {
 		System.err.println("Disconnected");
@@ -115,36 +144,49 @@ public class HistoricDump implements IStreamConsumerEvents {
 	 */
 	public void onInteraction(StreamConsumer c, Interaction i) {
 		try {
-			System.out.print(i.getStringVal("interaction.id"));
-		} catch (EInvalidData e) {
-			// Ignored
+			try {
+				_fw.append(i.getStringVal("interaction.id"));
+			} catch (EInvalidData e) {
+				// Ignored
+			}
+	
+			_fw.append(",\"");
+	
+			try {
+				_fw.append(i.getStringVal("interaction.created_at").replace(
+						"\"", "\\\""));
+			} catch (EInvalidData e) {
+				// Ignored
+			}
+	
+			_fw.append("\",\"");
+	
+			try {
+				_fw.append(i.getStringVal("interaction.author.username")
+						.replace("\"", "\\\""));
+			} catch (EInvalidData e) {
+				// Ignored
+			}
+	
+			_fw.append("\",\"");
+	
+			try {
+				_fw.append(i.getStringVal("interaction.content").replace(
+						"\"", "\\\"").replace('\n', ' '));
+			} catch (EInvalidData e) {
+				// Ignored
+			}
+	
+			_fw.append("\"\n");
+			_fw.flush();
+			
+			_counter += 1;
+			if (_counter % 1000 == 0) {
+				System.err.print("\r" + String.valueOf(_counter++));
+			}
+		} catch (IOException e) {
+			System.err.println("ERR: Failed to write interaction to the CSV file: " + e.getMessage());
 		}
-		
-		System.out.print(",\"");
-		
-		try {
-			System.out.print(i.getStringVal("interaction.created_at").replace("\"", "\\\""));
-		} catch (EInvalidData e) {
-			// Ignored
-		}
-		
-		System.out.print("\",\"");
-		
-		try {
-			System.out.print(i.getStringVal("interaction.author.username").replace("\"", "\\\""));
-		} catch (EInvalidData e) {
-			// Ignored
-		}
-		
-		System.out.print("\",\"");
-		
-		try {
-			System.out.print(i.getStringVal("interaction.content").replace("\"", "\\\""));
-		} catch (EInvalidData e) {
-			// Ignored
-		}
-		
-		System.out.println("\"");
 	}
 
 	/**
@@ -156,8 +198,7 @@ public class HistoricDump implements IStreamConsumerEvents {
 	 *            interaction The interaction data.
 	 * @throws EInvalidData
 	 */
-	public void onDeleted(StreamConsumer c, Interaction i)
-			throws EInvalidData {
+	public void onDeleted(StreamConsumer c, Interaction i) throws EInvalidData {
 		// For the purposes of this example we do nothing with deletes, but in
 		// your implementation you will need to properly process deletes by
 		// removing them from your storage system.
@@ -181,14 +222,25 @@ public class HistoricDump implements IStreamConsumerEvents {
 				// Failed to get the progress string, ignore it
 			}
 		} else {
-			System.err.println("STATUS: " + type);
+			try {
+				System.err.println("STATUS: " + type + " " + info.getStringVal("message"));
+			} catch (EInvalidData e1) {
+				System.err.println("STATUS: " + type); 
+			}
+			
 			try {
 				_h.reloadData();
-				System.err.println("STATUS: " + _h.getStatus() + " - " + String.valueOf(_h.getProgress()) + "% done");
+				System.err.println("STATUS: " + _h.getStatus() + " - "
+						+ String.valueOf(_h.getProgress()) + "% done");
+				if (_h.getStatus().equals("succeeded")) {
+					consumer.stop();
+				}
 			} catch (EInvalidData e) {
-				System.err.println("reloadData: EInvalidData: " + e.getMessage());
+				System.err.println("reloadData: EInvalidData: "
+						+ e.getMessage());
 			} catch (EAccessDenied e) {
-				System.err.println("reloadData: EAccessDenied: " + e.getMessage());
+				System.err.println("reloadData: EAccessDenied: "
+						+ e.getMessage());
 			} catch (EAPIError e) {
 				System.err.println("reloadData: EAPIError: " + e.getMessage());
 			}
@@ -198,8 +250,10 @@ public class HistoricDump implements IStreamConsumerEvents {
 	/**
 	 * Called when a warning is received in the data stream.
 	 * 
-	 * @param DataSift_StreamConsumer consumer The consumer object.
-	 * @param string message The warning message.
+	 * @param DataSift_StreamConsumer
+	 *            consumer The consumer object.
+	 * @param string
+	 *            message The warning message.
 	 */
 	public void onWarning(StreamConsumer consumer, String message)
 			throws EInvalidData {
@@ -209,8 +263,10 @@ public class HistoricDump implements IStreamConsumerEvents {
 	/**
 	 * Called when an error is received in the data stream.
 	 * 
-	 * @param DataSift_StreamConsumer consumer The consumer object.
-	 * @param string message The error message.
+	 * @param DataSift_StreamConsumer
+	 *            consumer The consumer object.
+	 * @param string
+	 *            message The error message.
 	 */
 	public void onError(StreamConsumer consumer, String message)
 			throws EInvalidData {
@@ -227,5 +283,10 @@ public class HistoricDump implements IStreamConsumerEvents {
 	 */
 	public void onStopped(StreamConsumer consumer, String reason) {
 		System.err.print("Stopped: " + reason);
+		try {
+			_fw.close();
+		} catch (IOException e) {
+			System.err.println("ERR: Failed to close the CSV file: " + e.getMessage());
+		}
 	}
 }
