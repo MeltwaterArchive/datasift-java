ad6d73367dd5c547b59a5ec96eaef3ad94205963
Jacek Artymiak <jacek.artymiak@datasift.com>
2012-12-17 14:43:41 +0000

    Streamlined autodoc generation process

--- doc-tools/make-docs.sh
+++ doc-tools/make-docs.sh
@@ -1,5 +1,30 @@
+#!/bin/sh -v
+if [ -z "$1" ]; then
+    echo 'You must run this script with branch name as its argument, e.g.'
+    echo 'sh ./make-docs.sh master'
+    exit
+fi
+echo 'working on branch '$1
+
+echo 'installing tools'
+sudo apt-get install git
+sudo apt-get install default-jdk
+echo 'making temporary directory'
+mkdir tmp
+cd tmp
+echo 'cloning repos'
+git clone https://github.com/datasift/datasift-java.git code
+git clone https://github.com/datasift/datasift-java.git gh-pages
+cd code
+git checkout $1
 cd ..
+cd gh-pages
+git checkout gh-pages
 javadoc -sourcepath ../master/src/ -windowtitle 'DataSift Java API' org.datasift org.json de.roderick
-#cp -a gh-pages/html/* ../gh-pages
-#cd ../gh-pages
-#mv .doc-tools doc-tools
+
+git add *
+git commit -m 'Updated to reflect the latest changes to '$1
+echo 'You are going to update the gh-pages branch to reflect the latest changes to '$1
+git push origin gh-pages
+echo 'finished'
+
