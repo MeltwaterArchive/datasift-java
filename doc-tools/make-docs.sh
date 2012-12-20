06cb0cbb34ddb11441e88b1bd415f917c8f89196
Rob Hubbard <rob.hubbard@datasift.com>
2012-12-20 13:13:07 +0000

    docathon 2012/12

--- doc-tools/make-docs.sh
+++ doc-tools/make-docs.sh
@@ -1,30 +1,37 @@
-#!/bin/sh -v
-if [ -z "$1" ]; then
-    echo 'You must run this script with branch name as its argument, e.g.'
-    echo 'sh ./make-docs.sh master'
-    exit
-fi
-echo 'working on branch '$1
+#!/bin/bash
+#-v
 
-echo 'installing tools'
+export BASE_DIR="$( cd "$( dirname $0 )/../../../.." && pwd )/"
+
+source ${BASE_DIR}ms-tools/doc-tools/docathon/sub/make-docs-util-defs.sh
+initialise $*
+
+### Java-specific parameters
+parameters "java"
+
+### installation of Java-specific tools
+message "installing tools"
 sudo apt-get install git
 sudo apt-get install default-jdk
-echo 'making temporary directory'
-mkdir tmp
-cd tmp
-echo 'cloning repos'
-git clone https://github.com/datasift/datasift-java.git code
-git clone https://github.com/datasift/datasift-java.git gh-pages
-cd code
-git checkout $1
-cd ..
-cd gh-pages
-git checkout gh-pages
-javadoc -sourcepath ../master/src/ -windowtitle 'DataSift Java API' org.datasift org.json de.roderick
 
-git add *
-git commit -m 'Updated to reflect the latest changes to '$1
-echo 'You are going to update the gh-pages branch to reflect the latest changes to '$1
-git push origin gh-pages
-echo 'finished'
+pre_build
 
+### Java-specific build steps
+
+#message "preparing to build documents"
+
+### (build/copy docs steps commbined in this case)
+(
+	message "building documents"
+	cd ${GH_PAGES_DIR} ; stop_on_error
+	javadoc -sourcepath ../master/src/ -windowtitle 'DataSift Java API' org.datasift org.json de.roderick ; stop_on_error
+) || error "stopped parent"
+
+(
+	cd ${GH_PAGES_DIR} ; stop_on_error
+	git add *
+) || error "stopped parent"
+
+post_build
+
+finalise
