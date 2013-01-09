bc58d9ba53bc0a0bbcad7fe810f9da3a3f9568e0
Jacek Artymiak <jacek.artymiak@datasift.com>
2013-01-09 12:17:49 +0000

    Fixes to the build process making it more javadoc-compliant.

--- doc-tools/make-docs.sh
+++ doc-tools/make-docs.sh
@@ -26,7 +26,19 @@ pre_build
 (
 	message "building documents"
 	cd ${GH_PAGES_DIR} ; stop_on_error
-	javadoc -sourcepath ../code/src/ -windowtitle 'DataSift Java API' org.datasift org.json de.roderick ; stop_on_error
+	cp doc-tools/overview.html ${CODE_DIR}src/ ; stop_on_error
+	javadoc \
+        	-d . \
+        	-use \
+        	-splitIndex \
+        	-overview ../code/src/overview.html \
+        	-doctitle 'DataSift Java API' \
+        	-header 'DataSift Java API' \
+        	-footer 'Copyright &copy; 2010-'`date +%Y`' <a href="http://datasift.com">DataSift</a>' \
+        	-bottom 'DataSift Java API' \
+        	-sourcepath ../code/src/ \
+        	-windowtitle 'DataSift Java API'  \
+        	org.datasift org.json de.roderick ; stop_on_error
 ) || error "stopped parent"
 
 (
@@ -34,6 +46,7 @@ pre_build
 	git add *
 ) || error "stopped parent"
 
+exit
 post_build
 
 finalise
