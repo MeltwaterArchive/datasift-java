4aad1cc36692ae656e660dd1732fdcc828863cb1
Andy Slack <andrew.slack@datasift.com>
2015-03-10 15:58:45 +0000

    Removed erroneous import to CLI class. DQ-516

--- src/main/java/com/datasift/client/pylon/PylonParametersData.java
+++ src/main/java/com/datasift/client/pylon/PylonParametersData.java
@@ -1,6 +1,5 @@
 package com.datasift.client.pylon;
 
-import com.datasift.client.cli.Interface;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
