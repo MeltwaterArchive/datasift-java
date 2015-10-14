f35a42f1f8395dc43cc31e6d158c14bcda6e93e7
Andy Slack <andrew.slack@datasift.com>
2015-10-14 23:20:51 +0100

    Added initial mock API & test for ODP API

--- src/test/java/com/datasift/client/mock/TestODPApiWithMocks.java
+++ src/test/java/com/datasift/client/mock/TestODPApiWithMocks.java
@@ -3,6 +3,8 @@ package com.datasift.client.mock;
 import com.datasift.client.DataSiftResult;
 import com.datasift.client.IntegrationTestBase;
 import com.datasift.client.mock.datasift.MockAnalysisApi;
+import com.datasift.client.mock.datasift.MockODPApi;
+import com.datasift.client.odp.ODPBatchResponse;
 import com.datasift.client.pylon.PylonStream;
 import com.datasift.client.pylon.PylonStreamStatus;
 import com.datasift.client.pylon.PylonTags;
@@ -28,37 +30,25 @@ import static org.junit.Assert.assertTrue;
 public class TestODPApiWithMocks extends IntegrationTestBase {
     private HiggsServer server;
     private Map<String, String> headers = new HashMap<>();
-    private MockAnalysisApi m = new MockAnalysisApi();
-    private String hash = new BigInteger(130, new Random()).toString(32);
-    private Map<String, Object> parameters = new HashMap<>();
-    private String status = new BigInteger(130, new Random()).toString(32);
-    private String message;
-    private long event_time;
-    private boolean success;
-    protected String csdl = "";
-    private double dpu = new Random().nextDouble();
-    private int interactions;
-    private int uniqueAuthors;
-    private int volume;
-    private long start, end;
-    private int remainingIndexCapacity;
-    private int remainingAccountCapacity;
-    private boolean reachedCapacity;
-    private List<Integer> results = new ArrayList<>();
-    protected long createdAt;
-    private ArrayList<String> tags = new ArrayList<String>();
+    private MockODPApi m = new MockODPApi();
+    private String testData = "{\"id\":\"first_interaction\"}\n{\"id\":\"second_interaction\"}";
 
     @Before
     public void setup() throws IOException, IllegalAccessException, Exception {
         server = MockServer.startNewServer();
-        config.host("localhost");
+        config.ingestionHost("localhost");
         config.setSslEnabled(false);
         config.port(server.getConfig().port);
         super.setup();
         headers.put("server", "nginx/0.8.55");
-        headers.put("x-ratelimit-limit", "10000");
-        headers.put("x-ratelimit-remaining", "10000");
-        headers.put("x-ratelimit-cost", "5");
+        headers.put("X-Ingestion-Request-RateLimit-Limit", "10000");
+        headers.put("X-Ingestion-Request-RateLimit-Remaining", "9999");
+        headers.put("X-Ingestion-Request-RateLimit-Reset", "57");
+        headers.put("X-Ingestion-Request-RateLimit-Reset-Ttl", "1442931984");
+        headers.put("X-Ingestion-Data-RateLimit-Limit", "10000000");
+        headers.put("X-Ingestion-Data-RateLimit-Remaining", "10000000");
+        headers.put("X-Ingestion-Data-RateLimit-Reset", "0");
+        headers.put("X-Ingestion-Data-RateLimit-Reset-Ttl", "1442931927");
         server.registerObjectFactory(new ObjectFactory(server) {
             public Object newInstance(Class<?> aClass) {
                 m.setHeaders(headers);
@@ -70,82 +60,17 @@ public class TestODPApiWithMocks extends IntegrationTestBase {
             }
         });
 
-        SecureRandom random = new SecureRandom();
-        csdl = "(fb.content any \"coffee\" OR fb.hashtags in \"tea\") AND fb.language in \"en\"";
-        hash = new BigInteger(130, random).toString(32);
-        dpu = Float.valueOf(String.valueOf(Math.random()));
-        tags.add("tag1");
-
-        m.setHash(hash);
-        m.setStatus(status);
-        m.setDpu(dpu);
-        m.setEnd(end);
-        m.setInteractions(interactions);
-        m.setReachedCapacity(reachedCapacity);
-        m.setRemainingIndexCapacity(remainingIndexCapacity);
-        m.setRemainingAccountCapacity(remainingAccountCapacity);
-        m.setStart(start);
-        m.setUniqueAuthors(uniqueAuthors);
-        m.setVolume(volume);
-        m.setCreatedAt(createdAt);
-        m.setParameters(parameters);
-        m.setStatus(status);
-        m.setResults(results);
-        m.setTags(tags);
+        m.setAccepted(12345);
+        m.setTotalMessageBytes(67890);
     }
 
     @Test
-    public void testIfUserCanGetStreamStatus() {
-        PylonStreamStatus statusResult = datasift.pylon().get(hash).sync();
-        assertTrue(statusResult.isSuccessful());
+    public void testIfUserCanBatchUpload() {
+        ODPBatchResponse batchResult = datasift.odp().batch("testsource", testData).sync();
+        assertTrue(batchResult.isSuccessful());
 
-        assertEquals(statusResult.getHash(), hash);
-        assertEquals(statusResult.getStart(), start);
-        assertEquals(statusResult.getEnd(), end);
-        assertEquals(statusResult.getVolume(), volume);
-        assertEquals(statusResult.getReachedCapacity(), reachedCapacity);
-        assertEquals(statusResult.getRemainingAccountCapacity(), remainingAccountCapacity);
-        assertEquals(statusResult.getRemainingIndexCapacity(), remainingIndexCapacity);
-        assertEquals(statusResult.getStart(), start);
-        assertEquals(statusResult.getStatus(), status);
-    }
-
-    @Test
-    public void testIfUserCanValidateCSDL() {
-        PylonValidation validationResult = datasift.pylon().validate(csdl).sync();
-        assertTrue(validationResult.isSuccessful());
-
-        assertEquals(validationResult.getDpu(), dpu, 0.1);
-        assertEquals(validationResult.getCreatedAt(), createdAt);
-    }
-
-    @Test
-    public void testIfUserCanCompileCSDL() {
-        PylonStream compilationResult = datasift.pylon().compile(csdl).sync();
-        assertTrue(compilationResult.isSuccessful());
-
-        assertEquals(compilationResult.getDpu(), dpu, 0.1);
-        assertEquals(compilationResult.getCreatedAt(), createdAt);
-        assertEquals(compilationResult.hash(), hash);
-    }
-
-    @Test
-    public void testIfUserCanStopDataStream() {
-        DataSiftResult stop = datasift.managedSource().stop(hash).sync();
-        assertTrue(stop.isSuccessful());
-    }
-
-    @Test
-    public void testIfUserCanStartDataStream() {
-        DataSiftResult start = datasift.pylon().start(hash).sync();
-        assertTrue(start.isSuccessful());
-    }
-
-    @Test
-    public void testIfUserCanGetTags() {
-        PylonTags tagsResult = datasift.pylon().tags(hash).sync();
-        assertTrue(tagsResult.isSuccessful());
-        assertEquals(tagsResult.getTags(), this.tags);
+        assertEquals(batchResult.getInteractionsProcessed(), 12345);
+        assertEquals(batchResult.getBytesProcessed(), 67890);
     }
 
     @After
--- src/test/java/com/datasift/client/mock/datasift/MockODPApi.java
+++ src/test/java/com/datasift/client/mock/datasift/MockODPApi.java
@@ -1,5 +1,6 @@
 package com.datasift.client.mock.datasift;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,12 +10,17 @@ public class MockODPApi {
     private long accepted;
     private long totalMessageBytes;
 
+    @POST
     @Path("testsource")
     public Map<String, Object> validate() {
         Map<String, Object> map = new HashMap<>();
         map.put("accepted", accepted);
         map.put("total_message_bytes", totalMessageBytes);
         return map;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
     }
 
     public void setAccepted(long accepted) {
