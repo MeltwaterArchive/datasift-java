e998fed600f1cbfb81a731dafc36095754e0c10f
Andy Slack <andrew.slack@datasift.com>
2015-11-02 00:53:46 +0000

    Reverting change to Limit class applied from intial refactoring due to IDE processing

--- src/main/java/com/datasift/client/accounts/Limit.java
+++ src/main/java/com/datasift/client/accounts/Limit.java
@@ -3,6 +3,9 @@ package com.datasift.client.accounts;
 import com.datasift.client.BaseDataSiftResult;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by andi on 24/04/15.
  */
