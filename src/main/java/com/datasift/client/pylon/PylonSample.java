6604f62c311d16dffe9ae01350862d3d98529fef
Andy Slack <andrew.slack@datasift.com>
2015-11-06 18:04:25 +0000

    Added PylonSample class for deserializing pylon/sample result JSON

---
+++ src/main/java/com/datasift/client/pylon/PylonSample.java
@@ -0,0 +1,34 @@
+package com.datasift.client.pylon;
+
+import com.datasift.client.BaseDataSiftResult;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PylonSample extends BaseDataSiftResult {
+    @JsonProperty("remaining")
+    protected int rateLimitRemaining;
+    @JsonProperty("reset_at")
+    protected int rateLimitResetTime;
+    @JsonProperty
+    protected List<PylonSampleInteractionItem> interactions = new ArrayList<PylonSampleInteractionItem>();
+
+    public PylonSample() { }
+
+    public int getRemainingRateLimit() { return this.rateLimitRemaining; }
+
+    public int getRateLimitResetTime() { return this.rateLimitResetTime; }
+
+    /**
+     * @return All available interactions. May be empty but will never be null
+     */
+    public List<PylonSampleInteractionItem> getInteractions() {
+        return interactions;
+    }
+
+    public Iterator<PylonSampleInteractionItem> iterator() {
+        return interactions.iterator();
+    }
+}
