4c4f37268c27e600c3ddc2e0e1329e2c95504fec
Andy Slack <andrew.slack@datasift.com>
2015-11-06 16:39:50 +0000

    Removed unnecessary inheritance from API result class for PylonQuery

--- src/main/java/com/datasift/client/pylon/PylonQuery.java
+++ src/main/java/com/datasift/client/pylon/PylonQuery.java
@@ -1,10 +1,9 @@
 package com.datasift.client.pylon;
 
-import com.datasift.client.BaseDataSiftResult;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class PylonQuery extends BaseDataSiftResult {
+public class PylonQuery {
     @JsonProperty
     protected String hash;
     @JsonProperty
