64ff29191c306e42a7f9b3e847fe0666ad0fdddf
zcourts <courtney@crlog.info>
2016-02-09 11:21:09 +0000

    Gherkin tests are still a WIP, shouldn't fail build yet
    Until the Gherkin tests are stable / we decided how to do the mocks
    we shouldn't fail the builds yet. Current approach is very fragile,
    so need to look into re-using the existing mock infrastructure
    or using something like Mockito

--- src/test/java/com/datasift/client/behavioural/CucumberRunnerTest.java
+++ src/test/java/com/datasift/client/behavioural/CucumberRunnerTest.java
@@ -1,18 +1,13 @@
 package com.datasift.client.behavioural;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
 //CHECKSTYLE:OFF
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber", "junit:target/cucumber/junit.xml"}
-        , snippets = SnippetType.CAMELCASE
-        , features = {"."}
-        //, strict = true
-)
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        plugin = {"pretty", "html:target/cucumber", "junit:target/cucumber/junit.xml"}
+//        , snippets = SnippetType.CAMELCASE
+//        , features = {"."}
+//        //, strict = true
+//)
 //CHECKSTYLE:ON
 public class CucumberRunnerTest {
 }
